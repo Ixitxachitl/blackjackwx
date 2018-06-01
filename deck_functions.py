181ffec471feb570b717d5c1568d0298c3b50631
nkoeppel <kramerfm@gmail.com>
2018-06-01 11:36:13 -0700

    Added suits

--- deck_functions.py
+++ deck_functions.py
@@ -10,53 +10,117 @@ class Card:
         if deckType == 'standard': # We'll create a single standard deck, only the card values and images are necessary, I've omitted suits
             deck = [] # Store the cards here
             tmp_id = 1 # This will set our card ids
-            #Now we add four of each card to the deck (because there are no suits)
-            for ace in range(4):
-                deck.append(Card(1,11,tmp_id,resource_path('A.png')))
-                tmp_id+=1
-            for two in range(4):
-                deck.append(Card(2,0,tmp_id,resource_path('2.png')))
-                tmp_id+=1
-            for three in range(4):
-                deck.append(Card(3,0,tmp_id,resource_path('3.png')))
-                tmp_id+=1
-            for four in range(4):
-                deck.append(Card(4,0,tmp_id,resource_path('4.png')))
-                tmp_id+=1
-            for five in range(4):
-                deck.append(Card(5,0,tmp_id,resource_path('5.png')))
-                tmp_id+=1
-            for six in range(4):
-                deck.append(Card(6,0,tmp_id,resource_path('6.png')))
-                tmp_id+=1
-            for seven in range(4):
-                deck.append(Card(7,0,tmp_id,resource_path('7.png')))
-                tmp_id+=1
-            for eight in range(4):
-                deck.append(Card(8,0,tmp_id,resource_path('8.png')))
-                tmp_id+=1
-            for nine in range(4):
-                deck.append(Card(9,0,tmp_id,resource_path('9.png')))
-                tmp_id+=1
-            for ten in range(4):
-                deck.append(Card(10,0,tmp_id,resource_path('10.png')))
-                tmp_id+=1
-            for jack in range(4):
-                deck.append(Card(10,0,tmp_id,resource_path('J.png')))
-                tmp_id+=1
-            for queen in range(4):
-                deck.append(Card(10,0,tmp_id,resource_path('Q.png')))
-                tmp_id+=1
-            for king in range(4):
-                deck.append(Card(10,0,tmp_id,resource_path('K.png')))
-                tmp_id+=1
+            deck.append(Card(1,11,tmp_id,resource_path('ah.png')))
+            tmp_id+=1
+            deck.append(Card(1,11,tmp_id,resource_path('ac.png')))
+            tmp_id+=1
+            deck.append(Card(1,11,tmp_id,resource_path('ad.png')))
+            tmp_id+=1
+            deck.append(Card(1,11,tmp_id,resource_path('as.png')))
+            tmp_id+=1
+            deck.append(Card(2,0,tmp_id,resource_path('2h.png')))
+            tmp_id+=1
+            deck.append(Card(2,0,tmp_id,resource_path('2c.png')))
+            tmp_id+=1
+            deck.append(Card(2,0,tmp_id,resource_path('2d.png')))
+            tmp_id+=1
+            deck.append(Card(2,0,tmp_id,resource_path('2s.png')))
+            tmp_id+=1
+            deck.append(Card(3,0,tmp_id,resource_path('3h.png')))
+            tmp_id+=1
+            deck.append(Card(3,0,tmp_id,resource_path('3c.png')))
+            tmp_id+=1
+            deck.append(Card(3,0,tmp_id,resource_path('3d.png')))
+            tmp_id+=1
+            deck.append(Card(3,0,tmp_id,resource_path('3s.png')))
+            tmp_id+=1
+            deck.append(Card(4,0,tmp_id,resource_path('4h.png')))
+            tmp_id+=1
+            deck.append(Card(4,0,tmp_id,resource_path('4c.png')))
+            tmp_id+=1
+            deck.append(Card(4,0,tmp_id,resource_path('4d.png')))
+            tmp_id+=1
+            deck.append(Card(4,0,tmp_id,resource_path('4s.png')))
+            tmp_id+=1
+            deck.append(Card(5,0,tmp_id,resource_path('5h.png')))
+            tmp_id+=1
+            deck.append(Card(5,0,tmp_id,resource_path('5c.png')))
+            tmp_id+=1
+            deck.append(Card(5,0,tmp_id,resource_path('5d.png')))
+            tmp_id+=1
+            deck.append(Card(5,0,tmp_id,resource_path('5s.png')))
+            tmp_id+=1
+            deck.append(Card(6,0,tmp_id,resource_path('6h.png')))
+            tmp_id+=1
+            deck.append(Card(6,0,tmp_id,resource_path('6c.png')))
+            tmp_id+=1
+            deck.append(Card(6,0,tmp_id,resource_path('6d.png')))
+            tmp_id+=1
+            deck.append(Card(6,0,tmp_id,resource_path('6s.png')))
+            tmp_id+=1
+            deck.append(Card(7,0,tmp_id,resource_path('7h.png')))
+            tmp_id+=1
+            deck.append(Card(7,0,tmp_id,resource_path('7c.png')))
+            tmp_id+=1
+            deck.append(Card(7,0,tmp_id,resource_path('7d.png')))
+            tmp_id+=1
+            deck.append(Card(7,0,tmp_id,resource_path('7s.png')))
+            tmp_id+=1
+            deck.append(Card(8,0,tmp_id,resource_path('8h.png')))
+            tmp_id+=1
+            deck.append(Card(8,0,tmp_id,resource_path('8c.png')))
+            tmp_id+=1
+            deck.append(Card(8,0,tmp_id,resource_path('8d.png')))
+            tmp_id+=1
+            deck.append(Card(8,0,tmp_id,resource_path('8s.png')))
+            tmp_id+=1
+            deck.append(Card(9,0,tmp_id,resource_path('9h.png')))
+            tmp_id+=1
+            deck.append(Card(9,0,tmp_id,resource_path('9c.png')))
+            tmp_id+=1
+            deck.append(Card(9,0,tmp_id,resource_path('9d.png')))
+            tmp_id+=1
+            deck.append(Card(9,0,tmp_id,resource_path('9s.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('10h.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('10c.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('10d.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('10s.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('jh.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('jc.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('jd.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('js.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('qh.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('qc.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('qd.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('qs.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('kh.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('kc.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('kd.png')))
+            tmp_id+=1
+            deck.append(Card(10,0,tmp_id,resource_path('ks.png')))
+            tmp_id+=1
             return deck #and return the list of cards
         elif deckType == 'aces':
             deck = [] # Store the cards here
             tmp_id = 1 # This will set our card ids
             #Now we add 52 aces to test with
             for ace in range(52):
-                deck.append(Card(1,11,tmp_id,resource_path('A.png')))
+                deck.append(Card(1,11,tmp_id,resource_path('as.png')))
                 tmp_id+=1
             return deck #and return the list of cards
         else:
